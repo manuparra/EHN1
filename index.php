16533ad0ced4d61aaebb595b79d1ac6a3db9c10d
Manuel Parra <manuparra@gmail.com>
2018-09-12 08:38:52 +0200

    First template

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#page-top">EHN1-NP Cluster</a>
+        </li>      
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#about">Cluster monitoring</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#services">protoDUNE/DUNE</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead d-flex">
+      <div class="container text-center my-auto">
+        <h1 class="mb-1">Welcome to ENH1-NP Cluster</h1>
+        <h3 class="mb-5">
+          <em>protoDUNE / DUNE</em>
+        </h3>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Cluster Monitoring</a>
+      </div>
+      <div class="overlay"></div>
+    </header>
+
+    <!-- About -->
+    <section class="content-section bg-light" id="about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
+            <p class="lead mb-5">This theme features a flexible, UX friendly sidebar menu and stock photos from our friends at
+              <a href="https://unsplash.com/">Unsplash</a>!</p>
+            <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">What We Offer</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Services -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">Computing infraestructure for: </h3>
+          <h2 class="mb-5">protoDune / DUNE</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-12 col-md-12 mb-5 mb-lg-0">
+            <p style="text-align:justify" class="text-faded mb-0">p3s stands for the ProtoDUNE Prompt Processing System.
+
+It was initially developed as a lightweight and easy-to-deploy tool for managing prompt processing workflows in the protoDUNE experiment at CERN. However, p3s does not contain any special logic or dependency related specifically to protoDUNE. It is content-agnostic and can be utilized for many other purposes.
+
+The design of p3s was inspired by pilot-based frameworks such as PanDA and Dirac, but p3s is a clean sheet development and prioritizes simplicity and component reuse over most other factors.</p>
+          </div>          
+      </div>
+    </section>
+
+    
+
+    
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+                <img src="https://cenf.web.cern.ch/sites/cenf.web.cern.ch/themes/cern_overwrite/img/cern-logo-large.png">
+        <ul class="list-inline mb-5">
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="http://facebook.com/cern">
+              <i class="icon-social-facebook"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="http://twitter.com/cern">
+              <i class="icon-social-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white" href="http://youtube.com/cern">
+              <i class="icon-social-youtube"></i>
+            </a>
+          </li>
+        </ul>
+
+
+        <p class="text-muted small mb-0">Copyright &copy; CERN 2018</p>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.min.js"></script>
+
+  </body>
+
+</html>
